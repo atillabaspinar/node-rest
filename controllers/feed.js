46b01321b47142891d45ae397663ae4c3086928d
atilla baspinar <abaspinar@isravision.com>
2019-11-05 17:18:32 +0100

    setup cors etc

---
+++ controllers/feed.js
@@ -0,0 +1,20 @@
+exports.getPosts = (req, res, next) => {
+    res.status(200).json({
+        posts: [
+            {
+                title: 'post 1',
+                content: 'this is the first post'
+            }
+        ]
+    });
+};
+
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    res.status(201).json({
+        message: 'created',
+        title: title,
+        content: content
+    });
+};
