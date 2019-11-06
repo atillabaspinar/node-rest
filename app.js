21746ca63170aceb9d38292f45700ed08267fe31
atilla baspinar <abaspinar@isravision.com>
2019-11-06 17:57:36 +0100

    posts

--- app.js
+++ app.js
@@ -1,19 +1,62 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const uuidv4 = require('uuid/v4');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        // cb(null, new Date().toISOString() + '-' + file.originalname);
+        cb(null, uuidv4() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 const feedRoutes = require('./routes/feed');
 app.use(bodyParser.json());
+app.use(multer({
+    storage: fileStorage,
+    fileFilter: fileFilter
+}).single('image'));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
  res.setHeader('Access-Control-Allow-Origin', '*');
- res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+ res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
- res.next();
+ next();
 });
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message: message});
+});
+
+mongoose.connect('mongodb://localhost/blog').then(
+    connection => {  
+        app.listen(8081);
+    }
+).catch(err => console.log(err));
+
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,20 +1,133 @@
+const fs = require('fs');
+const path = require('path');
+
+const {
+    validationResult
+} = require('express-validator/check');
+
+const mongoose = require('mongoose');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {
-                title: 'post 1',
-                content: 'this is the first post'
-            }
-        ]
+    const posts = Post.find().then(result => {
+        res.status(200).json({
+            posts: result
+        });
+    }).catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        err.message = "db error";
+        console.log(err);
+        next();
     });
 };
 
 exports.createPost = (req, res, next) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('validation failed' + errors.array());
+        error.statusCode = 422;
+        throw error;
+    }
     const title = req.body.title;
     const content = req.body.content;
-    res.status(201).json({
-        message: 'created',
+    if (!req.file) {
+        const err = new Error('no image provided');
+        err.statusCode = 422;
+        throw err;
+    }
+    const imageUrl = req.file.path.replace("\\" ,"/");
+    const post = new Post({
         title: title,
-        content: content
+        content: content,
+        imageUrl: imageUrl,
+        creator: {
+            name: 'atilla'
+        }
     });
+    post.save().then(result => {
+        res.status(201).json({
+            message: 'created',
+            post: result
+        });
+    }).catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        err.message = "db error";
+        console.log(err);
+        next();
+    });
+};
+
+exports.getPost = (req, res, next) => {
+    console.log('getPOst');
+    const id = req.params.postId;
+    Post.findById(id).then(post => {
+        if (!post) {
+            const err = new Error('could not found post')
+            err.statusCode = 404;
+            throw err;
+        }
+        res.status(200).json({
+            message: 'post found',
+            post: post
+        });
+    }).catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+            next();
+        }
+    });
+}
+
+exports.updatePost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('validation failed' + errors.array());
+        error.statusCode = 422;
+        throw error;
+    }    
+    const id = req.params.postId;
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+    if (req.file) {
+        imageUrl = req.file.path.replace("\\" ,"/");
+    }
+    if (!imageUrl) {
+        const err = new Error('no image provided');
+        err.statusCode = 422;
+        throw err;
+    }
+    Post.findById(id).then(post => {
+        if (!post) {
+            const err = new Error('could not found post')
+            err.statusCode = 404;
+            throw err;
+        }
+        post.title = title;
+        post.content = content;
+        if (post.imageUrl !== imageUrl) {
+            clearImage(post.imageUrl);
+        }
+        post.imageUrl = imageUrl;
+        
+        return post.save();        
+    }).then(result =>
+        res.status(200).json({message: 'post updated', post: result})
+    ).catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+            next();
+        }
+    });
+};
+
+clearImage = (filePath) => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err=> console.log(err));
 };
